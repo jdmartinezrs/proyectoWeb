c8feb2207e64e600fad0a94c6e70a92ca6e918b6
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-05-18 06:44:38 -0500

    feat: :sparkles: All Products section was done succesfully
    The Querys were called and elements joined well the composition

--- src/consultas.js
+++ src/consultas.js
@@ -14,37 +14,38 @@ export const getAbrigoInfo =async()=>{
     return dataUpdate
 }
 
-
-
-
-export const getAllcamisetaInformation = async() =>{
+export const getCamisetaInfo =async()=>{
     let res = await fetch("http://localhost:5501/camiseta")
-    let data = await res.json();
-    let dataUpdate = data.map(val=>{
-        return {
-            Name: val.nombre,
-            Image: val.imagen,
-            Price: val.precio,
+    let data = await res.json()
+    let dataUpdate = []
+    data.forEach(val =>{
+        dataUpdate.push({
+            name: val.nombre,
+            image: val.imagen,
+            price: val.precio,
+            id: val.id
 
-        }
+        })
     })
-    return dataUpdate;
-};
+    return dataUpdate
+}
 
-export const getAllpantalonInformation = async() =>{
+
+export const getPantalonInfo =async()=>{
     let res = await fetch("http://localhost:5501/pantalon")
-    let data = await res.json();
-    let dataUpdate = data.map(val=>{
-        return {
-            
-            Name: val.nombre,
-            Image: val.imagen,
-            Price: val.precio,
+    let data = await res.json()
+    let dataUpdate = []
+    data.forEach(val =>{
+        dataUpdate.push({
+            name: val.nombre,
+            image: val.imagen,
+            price: val.precio,
+            id: val.id
 
-        }
+        })
     })
-    return dataUpdate;
-};
+    return dataUpdate
+}
 
 
 
