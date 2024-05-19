eda6977164171e7892410ca626a62e24af748a16
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-05-19 00:59:21 -0500

    feat: :sparkles: Web componets were created
    the components were created for make easier the work with this proyect

---
+++ src/buttons.js
@@ -0,0 +1,158 @@
+import {LitElement,html,css} from "lit";
+import { AllAbrigoGotten } from "./my-abrigo";
+
+export class AllButtons extends LitElement{
+    constructor(){
+        super();
+    }
+    static styles = css`
+
+      
+    .aside {
+        padding: 2rem;
+        padding-top: 100px;
+        padding-right: 0;
+        color: var(--clr-white);
+        position: fixed;
+        top: 0;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items:flex-end;
+    }
+    
+
+    .logo {
+        padding-top:80px;
+        font-weight: 400;
+        font-size: 1.3rem;
+    }
+    
+    .menu {
+        display: flex;
+        flex-direction: column;
+        gap: .5rem;
+    }
+    
+    .boton-menu {
+        background-color: transparent;
+        border: 0;
+        color: var(--clr-white);
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        font-weight: 600;
+        padding: 1rem;
+        font-size: .85rem;
+        width: 100%;
+    }
+    .boton-menu.active {
+        background-color: var(--clr-white);
+        color: var(--clr-main);
+        border-top-left-radius: 1rem;
+        border-bottom-left-radius: 1rem;
+        position: relative;
+    }
+    
+    .boton-menu.active::before {
+        content: '';
+        position: absolute;
+        width: 1rem;
+        height: 2rem;
+        bottom: 100%;
+        right: 0;
+        background-color: transparent;
+        border-bottom-right-radius: .5rem;
+        box-shadow: 0 1rem 0 var(--clr-white);
+    }
+    
+    .boton-menu.active::after {
+        content: '';
+        position: absolute;
+        width: 1rem;
+        height: 2rem;
+        top: 100%;
+        right: 0;
+        background-color: transparent;
+        border-top-right-radius: .5rem;
+        box-shadow: 0 -1rem 0 var(--clr-white);
+    }
+    
+    .boton-menu > i.bi-hand-index-thumb-fill,
+    .boton-menu > i.bi-hand-index-thumb {
+        transform: rotateZ(90deg);
+    }
+    
+    .boton-carrito {
+        margin-top: 2rem;
+    }
+    
+    .numerito {
+        background-color: var(--clr-white);
+        color: var(--clr-main);
+        padding: .15rem .25rem;
+        border-radius: .25rem;
+    }
+    
+    .boton-carrito.active .numerito {
+        background-color: var(--clr-main);
+        color: var(--clr-white);
+    }
+    
+    .texto-footer {
+        color: var(--clr-main-light);
+        font-size: .85rem;
+    }
+  
+
+
+
+    `
+    render(){
+        return html` 
+        <aside>
+        <button class="close-menu" id="close-menu">
+            <i class="bi bi-x"></i>
+        </button>
+        <header>
+            <h1 class="logo">CampusShop</h1>
+        </header>
+        <nav>
+    
+        <ul class="menu">
+        <li>
+            <button id="todos" class="boton-menu boton-categoria active"><i class="bi bi-hand-index-thumb-fill"></i> Todos los productos</button>
+        </li>
+        <li>
+            <button id="abrigos" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Abrigos</button>
+        </li>
+        <li>
+            <button id="camisetas" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Camisetas</button>
+        </li>
+        <li>
+            <button id="pantalones" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Pantalones</button>
+        </li>
+        <li>
+            <a class="boton-menu boton-carrito" href="./views/carrito.html">
+                <i class="bi bi-cart-fill"></i> Carrito <span id="numerito" class="numerito">10</span>
+            </a>
+        </li>
+    </ul>
+    </nav>
+    <footer>
+    <p class="texto-footer">© 2024 CampusLands</p>
+    </footer>
+    </aside>
+     
+        
+        
+        `
+       
+  
+    }
+
+}
+
+customElements.define("my-buttons", AllButtons)
--- src/my-element.js
+++ src/my-element.js
@@ -1,6 +1,6 @@
 import { AllProducts } from "./allProducts";
 import { LitElement, html, css } from "lit";
-
+import { AllButtons } from "./buttons";
 
 
 class MyElement extends LitElement{
@@ -27,100 +27,7 @@ a {
     display: grid;
     grid-template-columns: 1fr 4fr;
     background-color: var(--clr-main);
-}
-
-aside {
-    padding: 2rem;
-    padding-right: 0;
-    color: var(--clr-white);
-    position: sticky;
-    top: 0;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-}
-
-.logo {
-    font-weight: 400;
-    font-size: 1.3rem;
-}
-
-.menu {
-    display: flex;
-    flex-direction: column;
-    gap: .5rem;
-}
-
-.boton-menu {
-    background-color: transparent;
-    border: 0;
-    color: var(--clr-white);
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-    font-weight: 600;
-    padding: 1rem;
-    font-size: .85rem;
-    width: 100%;
-}
-.boton-menu.active {
-    background-color: var(--clr-white);
-    color: var(--clr-main);
-    border-top-left-radius: 1rem;
-    border-bottom-left-radius: 1rem;
-    position: relative;
-}
-
-.boton-menu.active::before {
-    content: '';
-    position: absolute;
-    width: 1rem;
-    height: 2rem;
-    bottom: 100%;
-    right: 0;
-    background-color: transparent;
-    border-bottom-right-radius: .5rem;
-    box-shadow: 0 1rem 0 var(--clr-white);
-}
-
-.boton-menu.active::after {
-    content: '';
-    position: absolute;
-    width: 1rem;
-    height: 2rem;
-    top: 100%;
-    right: 0;
-    background-color: transparent;
-    border-top-right-radius: .5rem;
-    box-shadow: 0 -1rem 0 var(--clr-white);
-}
-
-.boton-menu > i.bi-hand-index-thumb-fill,
-.boton-menu > i.bi-hand-index-thumb {
-    transform: rotateZ(90deg);
-}
-
-.boton-carrito {
-    margin-top: 2rem;
-}
-
-.numerito {
-    background-color: var(--clr-white);
-    color: var(--clr-main);
-    padding: .15rem .25rem;
-    border-radius: .25rem;
-}
-
-.boton-carrito.active .numerito {
-    background-color: var(--clr-main);
-    color: var(--clr-white);
-}
-
-.texto-footer {
-    color: var(--clr-main-light);
-    font-size: .85rem;
+    
 }
 
 main {
@@ -129,6 +36,10 @@ main {
     margin-left: 0;
     border-radius: 2rem;
     padding: 3rem;
+    overflow-y: hidden; /* Oculta cualquier desbordamiento vertical */
+    overflow-y: scroll; 
+   height:940px;
+  
 }
 
 .titulo-principal {
@@ -400,39 +311,7 @@ render(){
             <i class="bi bi-list"></i>
         </button>
     </header>
-    <aside>
-    <button class="close-menu" id="close-menu">
-        <i class="bi bi-x"></i>
-    </button>
-    <header>
-        <h1 class="logo">CampusShop</h1>
-    </header>
-    <nav>
-
-    <ul class="menu">
-    <li>
-        <button id="todos" class="boton-menu boton-categoria active"><i class="bi bi-hand-index-thumb-fill"></i> Todos los productos</button>
-    </li>
-    <li>
-        <button id="abrigos" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Abrigos</button>
-    </li>
-    <li>
-        <button id="camisetas" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Camisetas</button>
-    </li>
-    <li>
-        <button id="pantalones" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Pantalones</button>
-    </li>
-    <li>
-        <a class="boton-menu boton-carrito" href="./views/carrito.html">
-            <i class="bi bi-cart-fill"></i> Carrito <span id="numerito" class="numerito">10</span>
-        </a>
-    </li>
-</ul>
-</nav>
-<footer>
-<p class="texto-footer">© 2024 CampusLands</p>
-</footer>
-</aside>
+  <my-buttons></my-buttons>
 <main>
 <h3 class="titulo-principal" id="titulo-principal">Todos los productos</h3>
 <div id="contenedor-productos" class="contenedor-productos">
