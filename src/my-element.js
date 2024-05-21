43e9515bd1bca1e699a096ba25c3e2b9c56306ff
jdmartinezrs <>
2024-05-20 19:32:52 -0500

    feat: :sparkles: Solving the buttoms isue

--- src/my-element.js
+++ src/my-element.js
@@ -1,4 +1,4 @@
-import { AllProducts } from "./allProducts";
+
 import "./my-pages.js"
 import { LitElement, html, css } from "lit";
 
@@ -9,376 +9,301 @@ class MyElement extends LitElement{
         super();
   }
 static styles = css`
-h1, h2, h3, h4, h5, h6, p, a, input, textarea, ul {
-    margin: 0;
-    padding: 0;
-}
-ul {
-    list-style-type: none;
-}
-a {
-    text-decoration: none;
-}
-.wrapper {
-    display: grid;
-    grid-template-columns: 1fr 4fr;
-    background-color: var(--clr-main);
-}
-aside {
-    padding: 2rem;
-    padding-right: 0;
-    color: var(--clr-white);
-    position: sticky;
-    top: 0;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-}
-.logo {
-    font-weight: 400;
-    font-size: 1.3rem;
-}
-.menu {
-    display: flex;
-    flex-direction: column;
-    gap: .5rem;
-}
-.boton-menu {
-    background-color: transparent;
-    border: 0;
-    color: var(--clr-white);
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-    font-weight: 600;
-    padding: 1rem;
-    font-size: .85rem;
+:host {
     width: 100%;
-}
-.boton-menu.active {
-    background-color: var(--clr-white);
-    color: var(--clr-main);
-    border-top-left-radius: 1rem;
-    border-bottom-left-radius: 1rem;
-    position: relative;
-}
-.boton-menu.active::before {
-    content: '';
-    position: absolute;
-    width: 1rem;
-    height: 2rem;
-    bottom: 100%;
-    right: 0;
-    background-color: transparent;
-    border-bottom-right-radius: .5rem;
-    box-shadow: 0 1rem 0 var(--clr-white);
-}
-.boton-menu.active::after {
-    content: '';
-    position: absolute;
-    width: 1rem;
-    height: 2rem;
-    top: 100%;
-    right: 0;
-    background-color: transparent;
-    border-top-right-radius: .5rem;
-    box-shadow: 0 -1rem 0 var(--clr-white);
-}
-.boton-menu > i.bi-hand-index-thumb-fill,
-.boton-menu > i.bi-hand-index-thumb {
-    transform: rotateZ(90deg);
-}
-.boton-carrito {
-    margin-top: 2rem;
-}
-.numerito {
-    background-color: var(--clr-white);
-    color: var(--clr-main);
-    padding: .15rem .25rem;
-    border-radius: .25rem;
-}
-.boton-carrito.active .numerito {
-    background-color: var(--clr-main);
-    color: var(--clr-white);
-}
-.texto-footer {
-    color: var(--clr-main-light);
-    font-size: .85rem;
-}
-main {
-    background-color: var(--clr-white);
-    margin: 1rem;
-    margin-left: 0;
-    border-radius: 2rem;
-    padding: 3rem;
-}
-.titulo-principal {
-    color: var(--clr-main);
-    margin-bottom: 2rem;
-}
-.contenedor-productos {
+    height: 100vh;
+    background: var(--color-secundario);
+    color: var(--color-primario);
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    overflow:hidden;
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 1rem;
-}
-.producto-imagen {
-    max-width: 100%;
-    border-radius: 1rem;
-}
-.producto-detalles {
-    background-color: var(--clr-main);
-    color: var(--clr-white);
-    padding: .5rem;
-    border-radius: 1rem;
-    margin-top: -2rem;
-    position: relative;
+    grid-template-columns: .1fr 1fr 4fr;
+    grid-template-rows: repeat(6, 1fr);
+    grid-template-areas:
+      ". logo img"
+      ". menus img"
+      ". menus img"
+      ". . img"
+      ". carrito img"
+      ". derechos img";
+  }
+
+  .div1 {
+    grid-area: logo;
+  }
+  .div1_h1 {
+    font-size: 2.5em;
+  }
+  #toggleMenu {
+    display: none;
+  }
+  .div2 {
+    grid-area: menus;
+  }
+  .menu{
+    height:100%;
+    width:100%;
+    display:flex;
+  }
+  .div2_ul {
+    height: 100%;
+    width:100%;
+    font-size: 1.5em;
     display: flex;
     flex-direction: column;
-    gap: .25rem;
-}
-.producto-titulo {
-    font-size: 1rem;
-}
-.producto-agregar {
-    border: 0;
-    background-color: var(--clr-white);
-    color: var(--clr-main);
-    padding: .4rem;
-    text-transform: uppercase;
-    border-radius: 2rem;
-    cursor: pointer;
-    border: 2px solid var(--clr-white);
-    transition: background-color .2s, color .2s;
-}
-.producto-agregar:hover {
-    background-color: var(--clr-main);
-    color: var(--clr-white);
-}
-/** CARRITO **/
-.contenedor-carrito {
-    display: flex;
-    flex-direction: column;
-    gap: 1.5rem;
-}
-.carrito-vacio,
-.carrito-comprado {
-    color: var(--clr-main);
-}
-.carrito-productos {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-}
-.carrito-producto {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: var(--clr-gray);
-    color: var(--clr-main);
-    padding: .5rem;
-    padding-right: 1.5rem;
-    border-radius: 1rem;
-}
-.carrito-producto-imagen {
-    width: 4rem;
-    border-radius: 1rem;
-}
-.carrito-producto small {
-    font-size: .75rem;
-}
-.carrito-producto-eliminar {
-    border: 0;
-    background-color: transparent;
-    color: var(--clr-red);
-    cursor: pointer;
-}
-.carrito-acciones {
-    display: flex;
-    justify-content: space-between;
-}
-.carrito-acciones-vaciar {
-    border: 0;
-    background-color: var(--clr-gray);
-    padding: 1rem;
-    border-radius: 1rem;
-    color: var(--clr-main);
-    text-transform: uppercase;
-    cursor: pointer;
-}
-.carrito-acciones-derecha {
-    display: flex;
-}
-.carrito-acciones-total {
-    display: flex;
-    background-color: var(--clr-gray);
-    padding: 1rem;
-    color: var(--clr-main);
-    text-transform: uppercase;
-    border-top-left-radius: 1rem;
-    border-bottom-left-radius: 1rem;
-    gap: 1rem;
-}
-.carrito-acciones-comprar {
-    border: 0;
-    background-color: var(--clr-main);
-    padding: 1rem;
-    color: var(--clr-white);
-    text-transform: uppercase;
-    cursor: pointer;
-    border-top-right-radius: 1rem;
-    border-bottom-right-radius: 1rem;
-}
-.header-mobile {
-    display: none;
-}
-.close-menu {
-    display: none;
-}
-.disabled {
-    display: none;
-}
-/*** MEDIA QUERIES ***/
-@media screen and (max-width: 850px) {
-    .contenedor-productos {
-        grid-template-columns: 1fr 1fr 1fr;
-    }
-}
-@media screen and (max-width: 675px) {
-    .contenedor-productos {
-        grid-template-columns: 1fr 1fr;
-    }
-}
-@media screen and (max-width: 600px) {
-    .wrapper {
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-    }
-    aside {
-        position: fixed;
-        z-index: 9;
-        background-color: var(--clr-main);
-        left: 0;
-        box-shadow: 0 0 0 100vmax rgba(0, 0, 0, .75);
-        transform: translateX(-100%);
-        opacity: 0;
-        visibility: hidden;
-        transition: .2s;
-    }
-    .aside-visible {
-        transform: translateX(0);
-        opacity: 1;
-        visibility: visible;
-    }
-    .boton-menu.active::before,
-    .boton-menu.active::after {
-        display: none;
-    }
-    main {
-        margin: 1rem;
-        margin-top: 0;
-        padding: 2rem;
-    }
-    .contenedor-productos {
-        grid-template-columns: 1fr 1fr;
-        
-    }
-    .header-mobile {
-        padding: 1rem;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-    .header-mobile .logo {
-        color: var(--clr-gray);
-    }
-    .open-menu, .close-menu {
-        background-color: transparent;
-        color: var(--clr-gray);
-        border: 0;
-        font-size: 2rem;
-        cursor: pointer;
-    }
-    .close-menu {
-        display: block;
-        position: absolute;
-        top: 1rem;
-        right: 1rem;
-    }
-    .carrito-producto {
-        gap: 1rem;
-        flex-wrap: wrap;
-        justify-content: flex-start;
-        padding: .5rem;
-    }
-    .carrito-producto-subtotal {
-        display: none;
-    }
-    .carrito-acciones {
-        flex-wrap: wrap;
-        row-gap: 1rem;
-    }
+    padding-inline-start: 0px;
+    margin-block-start: 0em;
+    margin-block-end: 0em;
+  }
+  .div2_ul_li {
+    height:100%;
+    width:100%;
+    list-style-type: none;
     
-}
-@media screen and (max-width: 400px) {
-    .contenedor-productos {
-        grid-template-columns: 1fr;
+  }
+  button{
+    height:90%;
+    width:100%;
+    text-indent: 0px;
+    text-shadow: none;
+    text-align: center;
+    cursor: pointer;
+    margin: 0em;
+    padding-block: 0px;
+    padding-inline: 0px;
+    border-width: 0px;
+    display:flex;
+    align-items: center;
+    font-size: 1em;
+    gap:10px;
+    background: var(--color-secundario);
+    color: var(--color-primario);
+  }
+  .opcion-seleccionada {
+    /*background: var(--color-primario;*/
+  }
+  .div3 {
+    grid-area: carrito;
+    height:50%;
+  }
+  .div3_1 {
+    font-size: 1.5em;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1.5em;
+  }
+  .bxs-cart {
+    font-size: 1.5em;
+  }
+  .div3_1_3 {
+    /*background: var(--color-primario;*/
+    width: 8%;
+    border: 3px solid var(--color-primario);
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #btnCarrito{
+    font-size:1.2em;
+  }
+  .div4 {
+    grid-area: derechos;
+  }
+  .div4_p {
+    font-size: 1.5em;
+  }
+  .div5 {
+    width: 100%;
+    height: 95%;
+    grid-area: img;
+    background: white;
+    border-radius: 3em;
+    margin-top: 1em;
+    margin-left: -1em;
+  }
+  .div5_p {
+    width: 100%;
+    height: 10vh;
+    font-size: 3em;
+    padding: 0.5em 0 0em 0.5em;
+    display: flex;
+    margin-block-start: 0em;
+    margin-block-end: 0em;
+  }
+  .div5_contenedor {
+    width: 100%;
+    height: 95%;
+    padding: 0 0 0 1.5em;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 1.8em;
+    max-height: 80vh;
+    overflow-y: scroll;
+  }
+
+  @media only screen and (max-width: 800px) {
+    body {
+      padding: 0.5em;
+      grid-template-columns: 1fr;
+      grid-template-areas:
+        "logo"
+        "menus"
+        "carrito"
+        "img"
+        "derechos";
     }
-}
+    .div2 nav {
+      display: none;
+      border: 1px solid var(--color-terciario);
+    }
+    #toggleMenu {
+      display: block;
+      font-size: 1.5em;
+      width: 3em;
+    }
+    .div2_ul {
+      padding-top: 1em;
+      gap: 0em;
+      margin-top: -1em;
+    }
+    .div2_ul_li_eleccion,
+    .div2_ul_li {
+    }
+    .div2_ul_li_eleccion {
+      background: none;
+    }
+    .div3_1 {
+      background: none;
+      padding-top: 2em;
+    }
+    .div4_p {
+      padding-top: 2em;
+    }
+    .div5 {
+      width: 95vw;
+      height: 100vh;
+      margin-top: 0.5em;
+      padding: 0;
+    }
+    .div5_p {
+      font-size: 1.5em;
+      padding: 1em 0 1.5em 1em;
+      display: flex;
+    }
+    .div5_contenedor {
+      height: 100%;
+      padding: 0 0 0 1em;
+      gap: 1em;
+      max-height: 75vh;
+      overflow-y: scroll;
+    }
+    .div5_1 {
+      width: 45%;
+      height: 35vh;
+      gap: 0.2em;
+    }
+    .div5_1 img {
+      width: 50%;
+      height: 30vh;
+    }
+    .div5_1_1 {
+      width: 100%;
+      font-size: 0.5em;
+      display: flex;
+      align-items: center;
+      line-height: 1.5em;
+    }
+    .div5_1_1_p {
+      width: 55%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      font-size: 1em;
+      margin-left: 1em;
+      line-height: 2em;
+    }
+    .button {
+      --width: 80px;
+      --height: 20px;
+    }
+    .icon svg {
+      width: 14px;
+      height: 14px;
+    }
+  }
 `;
+
 
 render(){
     return html`
-    <div class="wrapper">
-    <header class="header-mobile">
-        <h1 class="logo">CampusShop</h1>
-        <button class="open-menu" id="open-menu">
-            <i class="bi bi-list"></i>
-        </button>
-    </header>
-    <aside>
-    <button class="close-menu" id="close-menu">
-        <i class="bi bi-x"></i>
-    </button>
-    <header>
-        <h1 class="logo">CampusShop</h1>
-    </header>
-    <nav>
-    <ul class="menu">
-    <li id="">
-        <button id="todos" class="boton-menu boton-categoria active"><i class="bi bi-hand-index-thumb-fill"></i> todos los productos</button>
-    </li>
-    <li>
-        <button id="abrigos" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> abrigos</button>
-    </li>
-    <li>
-        <button id="camisetas" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> camisetas</button>
-    </li>
-    <li>
-        <button id="pantalones" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> pantalones</button>
-    </li>
-    <li>
-        <a class="boton-menu boton-carrito" href="./views/carrito.html">
-           
-            <i class="bi bi-cart-fill"></i> Carrito <span id="numerito" class="numerito">10</span>
-        </a>
-    </li>
-</ul>
-</nav>
-<footer>
-<p class="texto-footer">© 2024 CampusLands</p>
-</footer>
-</aside>
-<main>
-<h3 class="titulo-principal" id="titulo-principal">Todos los productos</h3>
-<div id="contenedor-productos" class="contenedor-productos">
-<my-component id="miComponente"></my-component>
-</div>
-</main>
-</div>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+    <head>
+        <link
+          href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+          rel="stylesheet"
+        />
+      </head>
+      <div class="div1">
+        <h1 class="div1_h1">CampusShop</h1>
+        <br />
+        <button id="toggleMenu">Menu</button>
+      </div>
+      <div class="div2">
+        <nav id="menu" class="menu">
+          <ul class="div2_ul">
+            <li class="div2_ul_li" id="todosProductos">
+              <button id="btnTodosProductos">
+                <i
+                  class='bx bx-closet'>
+                </i>
+                  <p class="div2_ul_li_p">Todos los productos</p>
+              </button>
+            </li>
+            <li class="div2_ul_li" id="abrigos">
+              <button id="btnAbrigos">
+                <i 
+                  class='bx bxs-thermometer'>
+                </i>
+                <p class="div2_ul_li_p">Abrigos</p>
+              </button>
+            </li>
+            <li class="div2_ul_li" id="camisetas">
+              <button id="btnCamisetas">
+                <i 
+                class='bx bxs-t-shirt'>
+                </i>
+                  <p class="div2_ul_li_p">Camisetas</p>
+                </button>
+            </li>
+            <li class="div2_ul_li" id="pantalones">
+              <button id="btnPantalones">
+                <i 
+                  class='bx bxs-arch'>
+                  </i>
+                <p class="div2_ul_li_p">Pantalones</p>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="div3">
+        <li class="div2_ul_li" id="carrito">
+          <button id="btnCarrito">
+          <i class="bx bxs-cart"></i>
+            <p class="div2_ul_li_p">Carrito</p>
+            <div class="div3_1_3">${this.numProductosEnCarrito}</div>
+          </button>
+        </li>
+      </div>
+      <div class="div4">
+        <p class="div4_p">© 2024</p>
+      </div>
+      <div class="div5">
+        <p class="div5_p"></p>
+        <my-component id="miComponente"></my-component>
+      </div>
  `;
 }
 
@@ -389,27 +314,27 @@ static get properties() {
   }
   connectedCallback() {
     super.connectedCallback();
-
+    
     this.updateComplete.then(() => {
-
-      const botonesDiv2 = this.shadowRoot.querySelectorAll('.boton-menu boton-categoria');
+      
+      const botonesDiv2 = this.shadowRoot.querySelectorAll('.div2_ul_li button');
       const miComponente = this.shadowRoot.querySelector('#miComponente');
-
+  
       const updateSelectedOption = (opcion) => {
         botonesDiv2.forEach(btn => btn.classList.remove('opcion-seleccionada'));
         opcion.classList.add('opcion-seleccionada');
         miComponente.opcionSeleccionada = opcion.querySelector('p').textContent.toLowerCase();
-
+        
         const textoOpcion = opcion.querySelector('p').textContent;
         const textoFormateado = textoOpcion.toLowerCase().replace(/\b\w/g, char => char.toUpperCase());
-
+        
         const div5_p = this.shadowRoot.querySelector('.div5_p');
         div5_p.textContent = textoFormateado;
       };
-
+      
       const btnTodosProductos = this.shadowRoot.querySelector('#btnCarrito');
       updateSelectedOption(btnTodosProductos);
-
+  
       botonesDiv2.forEach(boton => {
         boton.addEventListener('click', () => {
           updateSelectedOption(boton);
@@ -417,12 +342,17 @@ static get properties() {
       });
     });
   }
-
-
 }
 
+customElements.define("my-element", MyElement);
 
-
-
-
-customElements.define("my-element", MyElement)
+export async function actualizarCantidadProductosEnCarrito() {
+  try {
+    const response = await fetch('http://localhost:5501/carrito');
+    const carrito = await response.json();
+    this.numProductosEnCarrito=carrito.id.length
+    return this.numProductosEnCarrito;
+  } catch (error) {
+    console.error('Error al obtener el carrito:', error);
+  }
+}
--- src/my-pages.js
+++ src/my-pages.js
@@ -4,49 +4,40 @@ import"./my-abrigo.js"
 import"./my-pantalon.js"
 import"./my-camiseta.js"
 
+class MiComponente extends LitElement {
+  static get properties() {
+    return {
+      opcionSeleccionada: { type: String }
+    };
+  }
 
-class MiComponente extends LitElement{
-    static get properties(){
-        return{
-            opcionSeleccionada:
-            {type: String}
-        };
-    }
-
- constructor(){
+  constructor() {
     super();
-     this.opcionSeleccionada = "";
+    this.opcionSeleccionada = '';
+  }
 
- }
- render(){
+  render() {
     return html`
-    
-    <div class='magic'>
-    ${this.opcionSeleccionada === 'todos los productos'
-    ? html`<all-products></all-products>`
-    : ''}
-  ${this.opcionSeleccionada === 'abrigos'
-    ? html`<all-abrigogotten></all-abrigogotten>`
-    : ''}
-  ${this.opcionSeleccionada === 'camisetas'
-    ? html`<all-camisetagotten></all-camisetagotten>`
-    : ''}
-  ${this.opcionSeleccionada === 'pantalones'
-    ? html`<all-pantalongotten></all-pantalongotten>`
-    : ''}
-  ${this.opcionSeleccionada === 'carrito'
-    ? html`<my-cart></my-cart>`
-    : ''}
-</div>
-
-    ` 
- }
-
- 
-
-
-
-
+      <div class="contenido">
+        ${this.opcionSeleccionada === 'todos los productos'
+          ? html`<all-products></all-products>`
+          : ''}
+        ${this.opcionSeleccionada === 'abrigos'
+          ? html`<all-abrigogotten></all-abrigogotten>`
+          : ''}
+        ${this.opcionSeleccionada === 'camisetas'
+          ? html`<all-camisetagotten></all-camisetagotten>`
+          : ''}
+        ${this.opcionSeleccionada === 'pantalones'
+          ? html`<all-pantalongotten></all-pantalongotten>`
+          : ''}
+        ${this.opcionSeleccionada === 'carrito'
+          ? html`<my-cart></my-cart>`
+          : ''}
+      </div>
+    `;
+  }
 }
+
 customElements.define('my-component', MiComponente);
  
