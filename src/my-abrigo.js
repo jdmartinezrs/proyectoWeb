68669764aff377a2dbf09b2014f0c263a96731e3
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-05-22 13:46:51 -0500

    feat: :sparkles: Responsive design implemented

--- src/my-abrigo.js
+++ src/my-abrigo.js
@@ -10,52 +10,65 @@ export class AllAbrigoGotten  extends LitElement{
     
 static styles = css `
 
-:host{
-  padding-left:40px;
- 
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 40px;
-  
+:host {
+  padding-left: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 30px;
 }
+
 .card {
-    border-radius: 20px;
-    width: 250px;
-    padding: 20px;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-    background-color:  var(--clr-main-light);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  border-radius: 20px;
+  width: 250px;
+  padding: 20px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  background-color: var(--clr-main-light);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.product-image img {
+  width: 100%;
+  height: 300px;
+  border-radius: 10px;
+}
+
+.product-name {
+  color: #333;
+  font-size: 18px;
+  margin-top: 10px;
+}
+
+.product-price {
+  color: var(--clr-white);
+  font-size: 16px;
+}
+
+.add-button {
+  background-color: var(--clr-main);
+  color: white;
+  border: none;
+  padding: 10px;
+  text-align: center;
+  border-radius: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+@media (max-width: 768px) {
+  :host {
+    grid-template-columns: 1fr; /* Mostrar un solo producto por fila en dispositivos móviles */
+    padding-left: 0; /* Eliminar el relleno a la izquierda en dispositivos móviles */
   }
 
-  .product-image img {
-    width: 100%;
-    height: 300px;
-    border-radius: 10px;
+  .card {
+    width: calc(45% - 40px); /* Utilizar todo el ancho menos el relleno */
+    margin-right: auto;
+    margin-left: auto;
   }
+}
 
-  .product-name {
-    color: #333;
-    font-size: 18px;
-    margin-top: 10px;
-  }
-
-  .product-price {
-    color: var(--clr-white);
-    font-size: 16px;
-  }
-
-  .add-button {
-    background-color:  var(--clr-main);   
-    color: white;
-    border: none;
-    padding: 10px;
-    text-align: center;
-    border-radius: 20px;
-    cursor: pointer;
-    margin-top: 10px;
-  }
 
 `;
 constructor() {
